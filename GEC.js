eaf6db117af124f6badb356a69a64db4bb5ffdc4
adityaXkurama <raybandarkaraditya@gmail.com>
2025-04-07 20:52:58 +0530

    GEC

---
+++ GEC.js
@@ -0,0 +1,38 @@
+// GEC - Global Execution Context- 1.which is used to  how the global variables execute
+//                                  inside the js engine.
+                                //  2.Inside the browser js engine creates execution context           
+                                // (container)  for the globally declared variables
+
+                               
+                                // When the js engine takes the js file it scans the js file twice and splkit into 
+                                // 2 parts or phases first phase called variable phase 
+                                // second phase called execution phase
+
+                                //1.First phase 
+                                // in the first phase the variables which are hoisted are stored and sets initial value as undifined(NO INITIALIZATION PART)
+                               
+                                // 2.second phase
+                                // in the second phase again it scans the js file and assigns the initial value for the variables
+                                //  when a respective printing statement of the variable comes will get the values in the output.
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
