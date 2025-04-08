a1f21c656e5d6af5405b0718d3bdfc2698fac343
adityaXkurama <raybandarkaraditya@gmail.com>
2025-04-08 22:40:01 +0530

    Implicit type casting

---
+++ type-casting.js
@@ -0,0 +1,19 @@
+//     Type Casting
+
+// 1.Implicit  Type Casting
+// 2.Explicit  Type Casting
+
+console.log(10+10)
+
+console.log(10+10+"jb")
+
+
+let a=30+10+"5"-5
+console.log(a,typeof a)
+
+let b="5"-10
+console.log(b,typeof a)
+
+
+let c="9"
+console.log(c,typeof c)
