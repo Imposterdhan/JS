5287a01cdb8a83bfcf0fffa1cfa9b6f52c6b72eb
adityaXkurama <raybandarkaraditya@gmail.com>
2025-04-09 20:37:36 +0530

    explicit

---
+++ explicit-type-casting.js
@@ -0,0 +1,38 @@
+// explicit type casting
+
+
+// number to string
+
+let num=String(200)
+console.log(num,typeof num);
+
+// string to number
+
+let num2=Number("200")
+console.log(num2,typeof num2);
+
+// Boolean to Number
+
+let num3=Number(false)
+console.log(num3,typeof num3);
+
+// String to number
+
+let num4=Number("hello")
+console.log(num4,typeof num4);
+
+// String to boolean
+
+let num6=Boolean("hello")
+console.log(num6,typeof num6);
+
+// boolean to number
+
+let num5=Number(false)
+console.log(num3,typeof num3);
+
+console.log(String(Boolean("100")));
+
+
+console.log(Boolean(String("100")));
+
